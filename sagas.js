70ad53abd8f6e0f411dca31b411a3e19ba4dae44
雷永亮 <979700942@qq.com>
2017-11-23 14:35:54 +0800

    Update sagas.js

--- sagas.js
+++ sagas.js
@@ -5,18 +5,31 @@ export function* helloSaga() {
    console.log('hello saga')
 }
 
-export function* incrementAsync() {
-   yield delay(1000)
-   yield put({type: 'increase2'})
+export function* getData() {
+    let res = yield fetch('https://api.github.com/user')
+    console.log(res)
+    yield put({type: 'increase3'})
 }
 
-export function* watchIncrementAsync() {
-    yield takeEvery('incrementAsync', incrementAsync)
+export function* incrementAsync() {
+    yield delay(1000)
+    yield put({type: 'increase2'})
 }
+
+// export function* watchGetData() {
+//     yield takeEvery('getData', getData)
+// }
+//
+// export function* watchIncrementAsync() {
+//     yield takeEvery('incrementAsync', incrementAsync)
+// }
 
 export default function* rootSaga() {
-  yield all([
-    helloSaga(),
-    watchIncrementAsync()
-  ])
+  // yield all([
+  //   helloSaga(),
+  //   watchGetData(),
+  //   watchIncrementAsync()
+  // ])
+  yield takeEvery('getData', getData)
+  yield takeEvery('incrementAsync', incrementAsync)
 }
